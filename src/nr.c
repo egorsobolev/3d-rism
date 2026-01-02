c0ff497a0d807eda0a221a8c505334dd2bdc7e0e
Egor Sobolev <egor.sobolev@gmail.com>
2026-01-02 05:22:56 +0100

    OpenMP parallelization for solvers

--- src/nr.c
+++ src/nr.c
@@ -1,7 +1,7 @@
 #include <memory.h>
 #include <assert.h>
 #include <math.h>
-#include <cblas.h>
+#include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
@@ -11,17 +11,17 @@
 #define print  printf
 #define flush  fflush(stdin)
 
-#define narm  (5)
-#define sarm  (0.618033988749895)
+#define MAX_ARM  (5)
+#define SCALE_ARM  (0.618033988749895)
 
 
 /* + 5 * n * sizeof(float)*/
 int bicgstab(lneq_t *eq, int N, const float *b, float *x, float *tol, int *it)
 {
 	float *r, *h, *p, *v, *s;
-	float rTh, rTr, normr, norms, alpha, beta, omega, sTr;
-	float err, normb;
-	int its, maxit;
+	float rTh, rTr, sTr, hTv;
+	float sumb2, sumr2, sums2;
+	int its, maxit, more, exitcode;
 
 	/*
 	 r, h, p, v, s: float[N]
@@ -32,85 +32,152 @@ int bicgstab(lneq_t *eq, int N, const float *b, float *x, float *tol, int *it)
 	v = p + N;
 	s = v + N;
 
-	its = 0;
-	maxit = *it * 2;
-	normb = cblas_snrm2(N,b,1);
-	if (normb == 0.0f)
-		normb = 1.0f;
-	err = *tol * normb;
+	sumb2 = 0.0f;
+	rTh = 0.0f;
+	sumr2 = 0.0f;
+	#pragma omp parallel
+	{
+		int i, maxit, its, more;
+		float normb, normr, norms;
+		float err, omega, beta, alpha;
 
-	Jx(eq, x, r);
-	++its;
-
-	cblas_saxpy(N,-1.,b,1,r,1); /* r = Ax-b */
-	cblas_scopy(N,r,1,h,1);
-	cblas_scopy(N,r,1,p,1);
-
-	rTh=cblas_sdot(N,r,1,h,1); /* rho1 */
-	normr=cblas_snrm2(N,r,1);
-	if (normr <= err) {
-		*tol = normr / normb;
-		*it = its;
-		return 0;
-	}
-	while (its < maxit) {
-		Jx(eq,p,v);  /* Jx(eq, p, v) */
+		maxit = *it * 2;
+		its = 0;
+		more = 1;
+		
+		#pragma omp for reduction(+:sumb2)
+		for (i = 0; i < N; i++)
+			sumb2 += b[i] * b[i];
+		normb = sqrtf(sumb2);
+		if (normb == 0.0f)
+			normb = 1.0f;
+		err = *tol * normb;
+		Jx(eq, x, r);
 		++its;
 
-		alpha=rTh/cblas_sdot(N,h,1,v,1);
-		cblas_scopy(N,r,1,s,1);  /* s = h */
-		cblas_saxpy(N,-alpha,v,1,s,1); /* s = s - alpha * v */
-		norms=cblas_snrm2(N,s,1);
-		if (norms <= err) {
-			cblas_saxpy(N,-alpha,p,1,x,1);
-			*tol = norms / normb;
-			*it = its;
-			return 0;
+		#pragma omp for reduction(+:rTh, sumr2)
+		for (i = 0; i < N; i++) {
+			r[i] -= b[i];
+			p[i] = h[i] = r[i];
+			rTh += r[i] * h[i];
+			sumr2 += r[i] * r[i];
 		}
-
-		Jx(eq,s,r);
-		++its;
-		sTr=cblas_sdot(N,s,1,r,1);
-		rTr=cblas_sdot(N,r,1,r,1);
-		if ( fabs(sTr)<1e-40 || fabs(rTr)<1e-40 )
-			omega = 0.;
-		else
-			omega = sTr/rTr;
-
-		cblas_saxpy(N,-alpha,p,1,x,1);
-		cblas_saxpy(N,-omega,s,1,x,1);
-		cblas_sscal(N,-omega,r,1);
-		cblas_saxpy(N,1.f,s,1,r,1);
-
-		normr=cblas_snrm2(N,r,1);
+		normr = sqrtf(sumr2);
 		if (normr <= err) {
-			*tol = normr / normb;
-			*it = its;
-			return 0;
+			#pragma omp single
+			{
+				*tol = normr / normb;
+				*it = its;
+				exitcode = 0;
+			}
+			more = 0;
 		}
-		if (omega == 0.0f) {
-			*tol = normr / normb;
-			*it = its;
-			return 2;
+		while (more && its < maxit) {
+			Jx(eq,p,v);  /* Jx(eq, p, v) */
+			++its;
+			#pragma omp single nowait
+			hTv = 0.0f;
+			#pragma omp for reduction(+:hTv)
+			for (i = 0; i < N; i++)
+				hTv += h[i] * v[i];
+			alpha = rTh / hTv;
+			#pragma omp single nowait
+			sums2 = 0.0f;
+			#pragma omp for reduction(+:sums2)
+			for (i = 0; i < N; i++) {
+				s[i] = r[i] - alpha * v[i];
+				sums2 += s[i];
+			}
+			norms = sqrtf(sums2);
+			if (norms <= err) {
+				#pragma omp for
+				for (i = 0; i < N; i++)
+					x[i] -= alpha * p[i];
+				#pragma omp single
+				{
+					*tol = norms / normb;
+					*it = its;
+					exitcode = 0;
+				}
+				more = 0;
+				break;
+			}
+			Jx(eq,s,r);
+			++its;
+			#pragma omp single nowait
+			sTr = 0.0f;
+			#pragma omp single nowait
+			rTr = 0.0f;
+			#pragma omp for reduction(+:sTr, rTr)
+			for (i = 0; i < N; i++) {
+				sTr += s[i] * r[i];
+				rTr += r[i] * r[i];
+			}
+			if ( fabs(sTr)<1e-40 || fabs(rTr)<1e-40 )
+				omega = 0.;
+			else
+				omega = sTr/rTr;
+			#pragma omp single nowait
+			sumr2 = 0.0f;
+			#pragma omp for reduction(+:sumr2)
+			for (i = 0; i < N; i++) {
+				x[i] -= alpha * p[i] + omega * s[i];
+				r[i] = s[i] -omega * r[i];
+				sumr2 += r[i] * r[i];
+			}
+			normr = sqrtf(sumr2);
+			if (normr <= err) {
+				#pragma omp single
+				{
+					*tol = normr / normb;
+					*it = its;
+					exitcode = 0;
+				}
+				more = 0;
+				break;
+			}
+			if (omega == 0.0f) {
+				#pragma omp single
+				{
+					*tol = normr / normb;
+					*it = its;
+					exitcode = 2;
+				}
+				more = 0;
+				break;
+			}
+			beta=(alpha/omega)/rTh;
+			#pragma omp barrier
+			#pragma omp single nowait
+			rTh = 0.0f;
+			#pragma omp for reduction(+:rTh)
+			for (i = 0; i < N; i++)
+				rTh += r[i] * h[i];
+			if (rTh == 0.0f) {
+				#pragma omp single
+				{
+					*tol = normr / normb;
+					*it = its;
+					exitcode = 3;
+				}
+				more = 0;
+				break;
+			}
+			beta*=rTh;
+			#pragma omp for
+			for (i = 0; i < N; i++)
+				p[i] = beta * p[i] + r[i] - beta * omega * v[i];
 		}
-
-		beta=(alpha/omega)/rTh;
-		rTh=cblas_sdot(N,r,1,h,1);
-		if (rTh == 0.0f) {
-			*tol = normr / normb;
-			*it = its;
-			return 3;
+		if (more) {
+			#pragma omp single
+			{
+				*tol = rTr / normb;
+				*it = its;
+				exitcode = 1;
+			}
 		}
-		beta*=rTh;
-
-		cblas_sscal(N,beta,p,1);
-		cblas_saxpy(N,1.,r,1,p,1);
-		cblas_saxpy(N,-beta*omega,v,1,p,1);
 	}
-
-	*tol = rTr / normb;
-	*it = its;
-	return 1; /* no convergence */
+	return exitcode; /* no convergence */
 }
 
 /* Eisenstat, Walker */
@@ -153,10 +220,10 @@ double ftew_next(fterm_ew_t *f, double normd)
 /* 9 * n * sizeof(float) */
 int nr(eqoz_t *eq, double *t, double *tol, int *maxit)
 {
-	double *d, *s, *tN, norms, eta, lambda, err, errN, v;
+	double *d, *s, *tN, norms, eta, sumd2, sqrt_n;
 	float *b, *x, eps;
-	int n, m, i, j, flag, nlit, maxlit;
-	double lntm, nrtm, lntmi, nrtm_cpu;
+	int n, m, i, j, flag, nlit, maxlit, fail, narm;
+	double lntm, nrtm, lntmi, nrtm_cpu, err;
 	fterm_ew_t fterm;
 
 	nrtm_cpu = clock() / (double) CLOCKS_PER_SEC;
@@ -167,7 +234,7 @@ int nr(eqoz_t *eq, double *t, double *tol, int *maxit)
 	maxlit = 1000;
 
 	n = eq->solv.natv * eq->grid.nr;
-	v = sqrt(n);
+	sqrt_n = sqrt(n);
 
 	/*
 	 b, x: float[n]
@@ -178,9 +245,27 @@ int nr(eqoz_t *eq, double *t, double *tol, int *maxit)
 	tN = s + n;
 	b = (float *) (tN + n);
 	x = b + n;
+	
+	fail = 0;
+	sumd2 = 0.0;
+	#pragma omp parallel
+	{
+		int k;
 
-	eqoz(eq, t, d, eq->lneq.dcdg);
-	err = cblas_dnrm2(n, d, 1) / v;
+		eqoz(eq, t, d, eq->lneq.dcdg);
+
+		#pragma omp for reduction(+:sumd2)
+		for (k = 0; k < n; k++)
+			sumd2 += d[k] * d[k];
+		#pragma omp single nowait
+		err = sqrt(sumd2) / sqrt_n;
+		
+		#pragma omp for
+		for (k = 0; k < n; ++k) {
+			b[k] = (float) d[k];
+			x[k] = .0f;
+		}
+	}
 
 	print("||Z(t0)|| = %.1e\n", err);
 	print("    #     Nit F      t,s M   eta   ||Jx-Z||       ||Z||\n");
@@ -190,12 +275,8 @@ int nr(eqoz_t *eq, double *t, double *tol, int *maxit)
 
 		nlit = maxlit;
 		eta = ftew_next(&fterm, err);
-		eps = (float) eta;
-		for (j = 0; j < n; ++j) {
-			b[j] = (float) d[j];
-			x[j] = .0f;
-		}
 		lntmi = walltime() * 1e-6;
+		eps = (float) eta;
 		flag = bicgstab(&eq->lneq, n, b, x, &eps, &nlit);
 		lntmi = walltime() * 1e-6 - lntmi;
 		lntm += lntmi;
@@ -205,46 +286,78 @@ int nr(eqoz_t *eq, double *t, double *tol, int *maxit)
 			return 1;
 		}
 		norms = (double) eps;
-		for (j = 0; j < n; ++j) {
-			s[j] = (double) -x[j];
-		}
+		
+		#pragma omp parallel
+		{
+			int k, j;
+			double errN, lambda;
 
-		cblas_dcopy(n, t, 1, tN, 1);
-		cblas_daxpy(n, 1.0, s, 1, tN, 1);
-		eqoz(eq, tN, d, eq->lneq.dcdg);
-		errN = cblas_dnrm2(n, d, 1) / v;
+			#pragma omp for
+			for (k = 0; k < n; k++) {
+				s[k] = (double) -x[k];
+				tN[k] = t[k] + s[k];
+			}
 
-		j = 0;
-		lambda = 1.0;
-		while (j < narm && errN > err) {
-			lambda *= sarm;
-			cblas_dcopy(n, t, 1, tN, 1);
-			cblas_daxpy(n, lambda, s, 1, tN, 1);
 			eqoz(eq, tN, d, eq->lneq.dcdg);
-			errN = cblas_dnrm2(n, d, 1) / v;
-			++j;
-		}
+			#pragma single nowait
+			sumd2 = 0.0;
+			#pragma omp for reduction(+:sumd2)
+			for (k = 0; k < n; k++)
+				sumd2 += d[k] * d[k];
+			errN = sqrt(sumd2) / sqrt_n;
 
-		if (j >= narm) {
+			j = 0;
+			lambda = 1.0;
+			while (j < MAX_ARM && errN > err) {
+				lambda *= SCALE_ARM;
+				#pragma omp for
+				for (k = 0; k < n; k++)
+					tN[k] = t[k] + lambda * s[k];
+
+				eqoz(eq, tN, d, eq->lneq.dcdg);
+
+				#pragma omp single nowait
+				sumd2 = 0.0;
+				#pragma omp for reduction(+:sumd2)
+				for (k = 0; k < n; k++)
+					sumd2 += d[k] * d[k];
+				errN = sqrt(sumd2) / sqrt_n;
+				++j;
+			}
+
+			if (j >= MAX_ARM) {
+				/* no convergence of armigo*/
+				#pragma omp single nowait
+				fail = 1;
+			} else {
+				#pragma omp for
+				for (k = 0; k < n; k++) {
+					t[k] = tN[k];
+					b[k] = (float) d[k];
+					x[k] = .0f;
+				}
+				#pragma omp single nowait
+				err = errN;
+				#pragma omp single nowait
+				narm = j;
+			}
+		}
+		if (fail) {
 			/* no convergence of armigo*/
 			return 2;
 		}
-		cblas_dcopy(n, tN, 1, t, 1);
-		err = errN;
-
 		print("%5d %7d %1d %8.1lf %1d %5.2lf %10.6lf %11.6lf\n",
-		      i+1, nlit, flag, lntmi, j, eta, norms, err);
+		      i+1, nlit, flag, lntmi, narm, eta, norms, err);
 		flush;
 		++i;
 	}
-
 	*maxit = i;
 	*tol = err;
 
 	nrtm_cpu = clock() / (double) CLOCKS_PER_SEC - nrtm_cpu;
 	nrtm = walltime() * 1e-6 - nrtm;
 	print("NR: %.1lfs, BiCGStab: %.1lfs, CPU usage: %.1lf\n", nrtm, lntm, nrtm_cpu / nrtm);
-	print("||Z(tn)|| = %.1e\n", err);
+	print("||Z(tn)|| = %.1e\n", *tol);
 
 	return 0; /* OK */
 }
