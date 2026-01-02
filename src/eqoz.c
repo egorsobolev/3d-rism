2317bff69528f93ffe7e70f15192c75e0543f667
Egor Sobolev <egor.sobolev@gmail.com>
2026-01-02 05:22:43 +0100

    OpenMP parallelization for equations

--- src/eqoz.c
+++ src/eqoz.c
@@ -1,9 +1,9 @@
 #include "eqoz.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <memory.h>
 #include <math.h>
-#include <cblas.h>
 
 #include "utils.h"
 #include "asymp.h"
@@ -14,6 +14,7 @@ void hnc(int n, const double *uuv, const double *tuv, double *cuv, float *f)
 {
 	int i;
 	double a;
+	#pragma omp for
 	for (i = 0; i < n; ++i) {
 		a = exp(tuv[i] - uuv[i]) - 1.0;
 		f[i] = (float) a;
@@ -32,6 +33,7 @@ void plhnc(int n, const double *uuv, const double *tuv, double *cuv, float *f)
 {
 	int i;
 	double a, b;
+	#pragma omp for
 	for (i = 0; i < n; ++i) {
 		a = tuv[i] - uuv[i];
 		b = exp((a < 0.0) * a) - 1.0;
@@ -367,7 +369,7 @@ void rm_eqoz(eqoz_t *eq)
 void eqoz(eqoz_t *eq, double *tuv, double *d, float *f)
 {
 	int i, j, k, l, n, m;
-	double *r, *t, *s, *src, *dst, *b, a;
+	double *r, *t, *s, *b, a;
 	size_t nk, nr, natv;
 
 	nk = eq->grid.nk;
@@ -382,42 +384,35 @@ void eqoz(eqoz_t *eq, double *tuv, double *d, float *f)
 	s = t + m;
 
 	(*eq->closure)(n, eq->rism.uuv, tuv, r, f);
-
-	cblas_dcopy(n, r, 1, d, 1);
 	/*
 	 * d = r + rism.asymcr(:) * solv.charge;
-	 */
-	dst = d;
-	for (j = 0; j < natv; ++j) {
-		cblas_daxpy(nr, eq->solv.charge[j], eq->rism.asymcr, 1, dst, 1);
-		dst += nr;
-	}
-
-	/*
 	 * t = fftw_r2c(rism.wisdom, r);
 	 * t = t - rism.asymck(:) * solv.charge;
 	 */
-	src = d;
-	dst = t;
-	for (j = 0; j < natv; j++) {
-		cblas_dscal(nr, 1.0/nr, src, 1);
-		fft_r2c(3, eq->grid.fft_shape, src, (complex_double *) dst, 1.0);
-		cblas_daxpy(nk, -eq->solv.charge[j], eq->rism.asymck, 1, dst, 1);
-		cblas_dscal(nk, eq->solv.symc[j], dst, 1);
-
-		src += nr;
-		dst += nk;
-	}
-
+	#pragma omp for
+	for (i = 0; i < n; i++)
+		d[i] = (r[i] + eq->solv.charge[i / nr] * eq->rism.asymcr[i % nr]) / nr;
+	#pragma omp single
+	for (j = 0; j < natv; j++)
+		fft_r2c(3, eq->grid.fft_shape, d + j * nr, (complex_double *) (t + j * nk), 1.0);
+	#pragma omp for collapse(2)
+	for (j = 0; j < natv; j++)
+		for (i = 0; i < nk; i++) {
+			l = j * nk + i;
+			t[l] = (t[l] - eq->solv.charge[j] * eq->rism.asymck[i]) * eq->solv.symc[j];
+		}
 	/*
 	 * iga=rism.indga(fix(((1:rism.ngk)+1)/2));
 	 * s = reshape(sum(repmat(t,[1, 1, solv.natom]).*rism.xvva(iga,:,:),2),rism.ngk,solv.natom);
 	 */
-	memset(s, 0, m * sizeof(double));
+	#pragma omp for
+	for (i = 0; i < m; i++)
+		s[i] = 0;
 	l = 0;
 	for (j = 0; j < m; j += nk) {
 		/* side elemnts */
 		for (k = 0; k < j; k += nk) {
+			#pragma omp for
 			for (i = 0; i < nk; ++i) {
 				a = eq->solv.xvva[eq->solv.indga[i / 2] + l];
 				s[j + i] += a * t[k + i];
@@ -426,75 +421,51 @@ void eqoz(eqoz_t *eq, double *tuv, double *d, float *f)
 			l += eq->solv.lxvva;
 		}
 		/* diagonal elements */
+		#pragma omp for
 		for (i = 0; i < nk; ++i)
 			s[j + i] += t[j + i] * eq->solv.xvva[eq->solv.indga[i / 2] + l];
 		l += eq->solv.lxvva;
 	}
 	/*
-	memset(s, 0, m * sizeof(double));
-	dst = s;
-	l = 0;
-	for (j = 0; j < solv->natv; ++j) {
-		src = t;
-		for (k = 0; k < solv->natv; ++k) {
-			for (i = 0; i < grid->nk; ++i)
-				dst[i] += src[i] * solv->xvva[solv->indga[i / 2] + l];
-			src += grid->nk;
-			l += solv->lxvva;
-		}
-		dst += grid->nk;
-	}
-	*/
-
-	/*
 	 * s(1:2,:)=s(1:2,:)+rism.huvk0(1:2,:);
 	 * s = s + rism.asymhk(:) * (solv.charge_sp / solv.dielconst);
 	 */
-	dst = s;
-	src = eq->rism.huvk0;
+	#pragma omp single
 	for (j = 0; j < natv; ++j) {
-		cblas_dscal(nk, 1.0 / eq->solv.symc[j], dst, 1);
-		dst[0] += src[0];
-		dst[1] += src[1];
-
-		cblas_daxpy(nk, eq->solv.charge_sp[j], eq->rism.asymhk, 1, dst, 1);
-
-		dst += nk;
-		src += 2;
+		i = nk * j;
+		k = 2 * j;
+		s[i] += eq->rism.huvk0[k] * eq->solv.symc[j];
+		s[i + 1] += eq->rism.huvk0[k + 1] * eq->solv.symc[j];
 	}
-
+	#pragma omp for collapse(2)
+	for (j = 0; j < natv; j++)
+		for (i = 0; i < nk; i++) {
+				l = j * nk + i;
+				s[l] = s[l] / eq->solv.symc[j] + eq->solv.charge_sp[j] * eq->rism.asymhk[i];
+			}
 	/*
 	 * d = fftw_c2r(rism.wisdom, s);
 	 */
-	dst = d;
-	src = s;
-	for (j = 0; j < natv; j++) {
-		fft_c2r(3, eq->grid.fft_shape, (complex_double *) src, dst, 1.0);
-		src += nk;
-		dst += nr;
-	}
+	#pragma omp single
+	for (j = 0; j < natv; j++)
+		fft_c2r(3, eq->grid.fft_shape, (complex_double *) (s + j * nk), d + j * nr, 1.0);
 	/*
 	 * d = d - rism.asymhr * (solv.charge_sp / solv.dielconst);
 	 * d = -r - tuv
 	 */
-	dst = d;
-	src = r;
-	for (j = 0; j < natv; j++) {
-		cblas_daxpy(nr, -eq->solv.charge_sp[j], eq->rism.asymhr, 1, dst, 1);
-		cblas_daxpy(nr, -1.0, src, 1, dst, 1);
-		cblas_daxpy(nr, -1.0, tuv, 1, dst, 1);
-
-		dst += nr;
-		src += nr;
-		tuv += nr;
-	}
+	#pragma omp for collapse(2)
+	for (j = 0; j < natv; j++)
+		for (i = 0; i < nr; i++) {
+			l = j * nr + i;
+			d[l] -= eq->solv.charge_sp[j] * eq->rism.asymhr[i] + r[l] + tuv[l];
+		}
 }
 
 
 /* + 2 * n * sizeof(float) */
 void Jx(lneq_t *eq, float *x, float *r)
 {
-	float *d, *t, *src, *dst, *b, a;
+	float *d, *t, *b, a;
 	int i, j, k, l;
 	int n, m;
 	size_t nk, nr;
@@ -507,25 +478,26 @@ void Jx(lneq_t *eq, float *x, float *r)
 	d = eq->jx_data;
 	t = d + m;
 
-	for (i = 0; i < n; i++) {
+	#pragma omp for
+	for (i = 0; i < n; i++)
 		r[i] = eq->dcdg[i] * x[i] / nr;
+	#pragma omp single
+	for (j = 0; j < eq->natv; j++)
+		fftf_r2c(3, eq->grid->fft_shape, r + j * nr, (complex_float *) (d + j * nk), 1.0f);
+	/*
+	 d(:) = d(:) / symc
+	 t(:) = 0
+	*/
+	#pragma omp for
+	for (i = 0; i < m; i++) {
+		d[i] *= eq->symc[i / nk];
+		t[i] = 0;
 	}
-
-	src = r;
-	dst = d;
-	for (j = 0; j < eq->natv; j++) {
-		fftf_r2c(3, eq->grid->fft_shape, src, (complex_float *) dst, 1.0f);
-		cblas_sscal(nk, (float) eq->symc[j], dst, 1);
-
-		src += nr;
-		dst += nk;
-	}
-
-	memset(t, 0, m * sizeof(float));
 	l = 0;
 	for (j = 0; j < m; j += nk) {
 		/* side elemnts */
 		for (k = 0; k < j; k += nk) {
+			#pragma omp for
 			for (i = 0; i < nk; ++i) {
 				a = (float) eq->xvva[eq->indga[i / 2] + l];
 				t[j + i] += a * d[k + i];
@@ -534,35 +506,18 @@ void Jx(lneq_t *eq, float *x, float *r)
 			l += eq->lxvva;
 		}
 		/* diagonal elements */
+		#pragma omp for
 		for (i = 0; i < nk; ++i)
 			t[j + i] += d[j + i] * (float) eq->xvva[eq->indga[i / 2] + l];
 		l += eq->lxvva;
 	}
-	/*
-	memset(t, 0, m * sizeof(float));
-	dst = t;
-	l = 0;
-	for (j = 0; j < eq->natv; ++j) {
-		src = d;
-		for (k = 0; k < eq->natv; ++k) {
-			for (i = 0; i < eq->grid->nk; ++i)
-				dst[i] += src[i] * (float) eq->xvva[eq->indga[i / 2] + l];
-			src += eq->grid->nk;
-			l += eq->lxvva;
-		}
-		dst += eq->grid->nk;
-	}
-	*/
-	cblas_sscal(n, -1.0f * nr, r, 1);
-	cblas_saxpy(n, -1.0f, x, 1, r, 1);
-	dst = r;
-	src = t;
-	for (j = 0; j < eq->natv; j++) {
-		cblas_sscal(nk, 1.0f / (float) eq->symc[j], src, 1);
-		fftf_c2r(3, eq->grid->fft_shape, (complex_float *) src, d, 1.0f);
-		cblas_saxpy(nr,1.0f,d,1,dst,1);
-
-		src += nk;
-		dst += nr;
-	}
+	#pragma omp for
+	for (i = 0; i < m; i++)
+		t[i] /= eq->symc[i / nk];
+	#pragma omp single
+	for (j = 0; j < eq->natv; j++)
+		fftf_c2r(3, eq->grid->fft_shape, (complex_float *) (t + j * nk), d + j * nr, 1.0f);
+	#pragma omp for
+	for (i = 0; i < n; i++)
+		r[i] = d[i] - x[i] - nr * r[i];
 }
